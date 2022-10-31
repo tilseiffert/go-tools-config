dfe0dcf81c18b5e3ecc337741b762c1c2290e764
Tilmann Seiffert <mail@tilseiffert.de>
2022-10-31 21:36:20 +0100

    improve example
    Signed-off-by: Tilmann Seiffert <mail@tilseiffert.de>

--- example/config/main.go
+++ example/config/main.go
@@ -3,20 +3,26 @@ package main
 import (
 	"fmt"
 
+	"github.com/spf13/viper"
 	"github.com/tilseiffert/go-tools-config/config"
 )
 
 func main() {
 
-	fmt.Println("Hello World")
-
-	config.Init(config.Configuration{
+	// prepare
+	configuration := config.Configuration{
 		Options: []config.Option{
 			{
-				Name:    "ONE",
-				Default: 1,
+				Name:    "name",
+				Default: "Tilmann",
 			},
 		},
-	})
+	}
+
+	// initialize
+	config.Init(configuration)
+
+	// do your stuff
+	fmt.Printf("Hello %s\n", viper.GetString(configuration.Options[0].Name))
 
 }
